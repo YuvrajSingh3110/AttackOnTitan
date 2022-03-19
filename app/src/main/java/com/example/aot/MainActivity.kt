73d89a64970ea64741abd2650aef86eea92c5d8e
Yuvraj Singh <ysrishu1031@gmail.com>
2022-03-19 22:34:38 +0530

    fixed splash screen and added nav bar

--- app/src/main/java/com/example/aot/MainActivity.kt
+++ app/src/main/java/com/example/aot/MainActivity.kt
@@ -1,34 +1,31 @@
 package com.example.aot
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.view.WindowManager
-import android.view.animation.AnimationUtils
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_AOT)
         setContentView(R.layout.activity_main)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_slide)
-
-
-        Handler().postDelayed ({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-
-        }, 3000)
-    }
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN
+//        )
+//        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_slide)
+//
+//
+//        Handler().postDelayed ({
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//
+//        }, 3000)
+   }
 }
 
 
